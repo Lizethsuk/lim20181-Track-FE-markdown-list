0c81c2825753bd97198eabfa2ad837adabb59b63
Lizethsuk <julia.sucasacai@gmail.com>
2018-08-27 11:13:01 -0500

    close #2

--- index.js
+++ index.js
@@ -1,24 +1,94 @@
 #!/usr/bin/env node
 
-const [,, ... args]=process.argv
-console.log(`lalala lalammm${args}`);
+const [, , ...args] = process.argv;
+const fs = require('fs');
+const path = require('path');
+const recursive = require('recursive-readdir');
+const dirOrFile = path.resolve(args[0])
+const option={
+'validate': false,
+'stats':false
+}
 
-const showdown = require('showdown');
-const convertidor= new showdown.Converter();
-const text='#hola, rebaja';
-const html= convertidor.makeHtml(text);
-console.log(html);
+const arr = [];
 
-const fs= require('fs');
-fs.readFile('README.md','utf8', (error, text) => {
-    // => [Error: EISDIR: illegal operation on a directory, read <directory>]
-    if (error)
-    throw error;
-    const textoMd= text;
+// const funcLinks=(dirOrFile)=>{
+if (path.extname(dirOrFile) === '.md') {
+  // console.log(dirOrFile)
+  fs.readFile(dirOrFile, 'utf8', (error, text) => {
+    // console.log('sasa')
+
     const showdown = require('showdown');
-const convertidor= new showdown.Converter();
-const html= convertidor.makeHtml(textoMd);
-console.log(html);
-  });
-  
+    const convertidor = new showdown.Converter();
+    const html = convertidor.makeHtml(text);
+    // console.log(html)
+    const urlRegex = /<a href="([^"]*)">([^<]*)<.*/g;
+    ///
+    ///((\bhttps?:\/\/)|(\bwww\.))\S*/g
+    const url = html.match(urlRegex);
+    // const regexComent = /(?:(?<=\>).+?(?=\<))/g;
+    // /(?:(?<=\[).+?(?=\]))/g;
+    // const arrComent= text.match(regexComent)
+    // arrComent.forEach(oneComent=>{
+    //   if(oneComent.trim()!=''){
+    //     console.log(oneComent);
+    //     }
+    // })
+    // console.log(url)
+    if (url != null) {
+      for (let i = 0; i < url.length; i++) {
+        const splitcom = url[i].split('"');
+
+        const links = splitcom[1];
+        // console.log(splitComen)
+        console.log(links)
+        const axios = require('axios');
+        axios.get(links)
+          .then(function (response) {
+            console.log(response.status, response.statusText);
+          })
+          .catch(function (error) {
+            // console.log(error);
+          })
+          .then(function () {
+          });
+      }
+    }
+  })
+} else if (path.extname(dirOrFile) === '') {
+ 
+  recursive(dirOrFile,[ignoreFunc], function (err, files) {
+    files.forEach(file => {
+      if (path.extname(file) === '.md') {
+        fs.readFile(file, 'utf8', (error, text) => {
+          // console.log('sasa')
+          const showdown = require('showdown');
+          const convertidor = new showdown.Converter();
+          const html = convertidor.makeHtml(text);
+          const urlRegex = /<a href="([^"]*)">([^<]*)<.*/g;
+          const url = html.match(urlRegex);
+          const regexComent = /(?:(?<=\[).+?(?=\]))/g
+          // console.log(text.match(regexComent))
+          if (url != null) {
+            for (let i = 0; i < url.length; i++) {
+              const splitcom = url[i].split('"');
+              const links = splitcom[1];
+              // console.log(dirOrFile + file)
+              console.log(links)
+            }
+          }
+        })
+      }
+    })
+
+    // `files` es una matriz de rutas de archivos 
+
+  })
+}
+// }
+
+function ignoreFunc(file, stats) {
+  return stats.isDirectory() && path.basename(file) == "node_modules";
+};
+
 
---
+++ test.md
@@ -0,0 +1,23 @@
+# Markdown Links
+
+## Preámbulo
+
+[Markdown](https://es.wikipedia.org/wiki/Markdown) es un lenguaje de marcado
+ligero muy popular entre developers. Es usado en muchísimas plataformas que
+manejan texto plano (GitHub, foros, blogs, ...), y es muy común
+encontrar varios archivos en ese formato en cualquier tipo de repositorio
+(empezando por el tradicional `README.md`).
+
+$ md-links ./some/example.md
+./some/example.md http://algo.com/2/3/ Link a algo
+./some/example.md https://otra-cosa.net/algun-doc.html algún doc
+./some/example.md http://google.com/ Google
+```
+
+### Otros recursos
+
+
+- [learnyounode](https://github.com/workshopper/learnyounode)
+- [how-to-npm](https://github.com/workshopper/how-to-npm)
+- [promise-it-wont-hurt](https://github.com/stevekane/promise-it-wont-hurt)
+
